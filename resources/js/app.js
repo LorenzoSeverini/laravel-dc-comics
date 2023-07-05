2d5d923f7d38a1d0582a3030515134172d7f375f
LorenzoSeverini <lorenzo.seve98@gmail.com>
2023-07-05 18:08:07 +0200

    Feat: trying to solve the bonus

--- resources/js/app.js
+++ resources/js/app.js
@@ -12,3 +12,32 @@ import * as bootstrap from 'bootstrap'
 //Informo Vite di processare le immagini presenti in /resources/img
 //Documentazione: https://laravel.com/docs/9.x/vite#blade-processing-static-assets
 // import.meta.glob(['../img/**'])
+
+
+const deleteButton = document.getElementById('deleteButton');
+const comicId = deleteButton.getAttribute('data-comic-id');
+
+deleteButton.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    const confirmed = confirm('Are you sure you want to delete this Comic?');
+
+    if (confirmed) {
+        fetch(`/comic/${comicId}`, {
+            method: 'DELETE',
+            headers: {
+                'X-CSRF-TOKEN': document.querySelector('meta[name="csrf-token"]').getAttribute('content')  // Laravel's CSRF protection
+            }
+        })
+        .then(response => {
+            if (response.ok) {
+                // Comic deleted successfully
+                window.location.href = '/comic';
+            } else {
+                // Error occurred while deleting comic
+                console.error('Error deleting comic:', response.status);
+            }
+        })
+    }
+});
+
