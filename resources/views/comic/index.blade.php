0fa7e668bbf8d54d69f396561a81ffe240057a93
LorenzoSeverini <lorenzo.seve98@gmail.com>
2023-07-05 16:40:13 +0200

    feat: link to go to create page for compile form

--- resources/views/comic/index.blade.php
+++ resources/views/comic/index.blade.php
@@ -1,8 +1,10 @@
 @extends('layouts.app')
-
 @section('content')
 <div class="container my-3">
     <h1>Dc Comics List:</h1>
+    {{-- link to create a new comic --}}
+    <a href="{{ route('comic.create') }}" class="btn btn-primary">Create new Comic</a>
+    {{-- list of comics --}}
     <div class="row row-cols-1 row-cols-md-3">
         @foreach ($comics as $comic)
             <div class="card m-3">
@@ -11,7 +13,7 @@
                     <h2 class="card-title">{{ $comic->title }}</h2>
                 </div>
                 <div class="card-footer">
-                    <a href="{{ route('comics.show', $comic->id )}}" class="btn btn-primary">Details</a>
+                    <a href="{{ route('comic.show', $comic->id )}}" class="btn btn-primary">Details</a>
                 </div>
             </div>
         @endforeach
